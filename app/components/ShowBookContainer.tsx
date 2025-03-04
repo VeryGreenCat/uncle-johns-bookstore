4cc89ad9a31c33adf2001afb51cd0d1791fbdb39
mmmmmmmmmm04 <narinrat.rs@gmail.com>
2025-03-04 17:52:50 +0700

    edit 4 column to 5 column

--- app/components/ShowBookContainer.tsx
+++ app/components/ShowBookContainer.tsx
@@ -29,7 +29,7 @@ const ShowBookContainer = ({
           Show All
         </button>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4 mt-4">
         {books.map((book) => (
           <Book key={book.id} {...book} />
         ))}
