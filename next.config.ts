6541cf64de087917fa6aa603a7e108ed71898863
VeryGreenCat <best.kasidis@gmail.com>
2025-10-07 13:08:27 +0700

    next.config changes for new supabase url; the old one expired

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: [
-      "kcshzfsfcfotmvkwgoqm.supabase.co",
-      "promptpay.io", // Add this line
-    ],
-  },
-  typescript: {
-    // ปิดการตรวจสอบ TypeScript ระหว่างการ build
-    ignoreBuildErrors: true,
-  },
-  reactStrictMode: false,
+	images: {
+		domains: [
+			"yynfzvxxyvivwcvvzk.supabase.co",
+			"promptpay.io", // Add this line
+		],
+	},
+	typescript: {
+		// ปิดการตรวจสอบ TypeScript ระหว่างการ build
+		ignoreBuildErrors: true,
+	},
+	reactStrictMode: false,
 };
 
 export default nextConfig;
