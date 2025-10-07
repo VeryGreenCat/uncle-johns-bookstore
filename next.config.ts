ea09ee85fd76c79ab615fff8c1817543e75f9683
VeryGreenCat <best.kasidis@gmail.com>
2025-10-07 13:22:51 +0700

    config fix : wrong url and update Next.js 15+ expects images.remotePatterns.

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 	images: {
-		domains: [
-			"yynfzvxxyvivwcvvzk.supabase.co",
-			"promptpay.io", // Add this line
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "yynfzvxxyvivwcpcvvzk.supabase.co",
+				pathname: "/storage/v1/object/public/**",
+			},
+			{
+				protocol: "https",
+				hostname: "promptpay.io",
+				pathname: "/**",
+			},
 		],
 	},
 	typescript: {
-		// ปิดการตรวจสอบ TypeScript ระหว่างการ build
 		ignoreBuildErrors: true,
 	},
 	reactStrictMode: false,
